557cba82937ad27bc03706d01d1202eafae5b497
Tunc-Senel <senel.tunc@gmail.com>
2026-05-19 22:07:32 +0200

    feat: animate chicken enemies

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -1,12 +1,30 @@
 class Chicken extends MovableObject {
-    x = 500;
-    y = 250;
-    img;
-    width = 100;
-    height = 150;
+    y = 355;
+    width = 75;
+    height = 65;
+
+    IMAGES_WALKING = [
+        "assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+        "assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+        "assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png"
+    ]
 
     constructor() {
         super().loadImage("assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+        this.loadImages(this.IMAGES_WALKING);
+        this.x = 300 + (Math.random() * 500);
+        this.speed = 0.15 + Math.random() * 0.25;
+        this.animate();
     }
 
+    animate() {
+        setInterval( () => {
+            this.moveLeft();
+        }, 1000 / 60);
+        
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING);
+        }, 200);
+    }
+   
 }
--- levels/level1.js
+++ levels/level1.js
@@ -1,5 +1,9 @@
 const level1 = new Level(
     [
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
         new Chicken()
     ],
     [
