7695f85bdbe9e86fc4ed13c52f4e64285193ef5e
Tunc-Senel <senel.tunc@gmail.com>
2026-05-21 14:43:50 +0200

    feat: extend level background for camera movement

--- levels/level1.js
+++ levels/level1.js
@@ -7,10 +7,46 @@ const level1 = new Level(
         new Chicken()
     ],
     [
-        new BackgroundObject("assets/img/5_background/layers/air.png", 0, 80),
-        new BackgroundObject("assets/img/5_background/layers/3_third_layer/1.png", 0, 80),
-        new BackgroundObject("assets/img/5_background/layers/2_second_layer/1.png", 0, 80),
-        new BackgroundObject("assets/img/5_background/layers/1_first_layer/1.png", 0, 80)
+    new BackgroundObject("assets/img/5_background/layers/air.png", -720, 80),
+    new BackgroundObject("assets/img/5_background/layers/3_third_layer/2.png", -720, 80),
+    new BackgroundObject("assets/img/5_background/layers/2_second_layer/2.png", -720, 80),
+    new BackgroundObject("assets/img/5_background/layers/1_first_layer/2.png", -720, 80),
+
+    new BackgroundObject("assets/img/5_background/layers/air.png", 0, 80),
+    new BackgroundObject("assets/img/5_background/layers/3_third_layer/1.png", 0, 80),
+    new BackgroundObject("assets/img/5_background/layers/2_second_layer/1.png", 0, 80),
+    new BackgroundObject("assets/img/5_background/layers/1_first_layer/1.png", 0, 80),
+
+    new BackgroundObject("assets/img/5_background/layers/air.png", 720, 80),
+    new BackgroundObject("assets/img/5_background/layers/3_third_layer/2.png", 720, 80),
+    new BackgroundObject("assets/img/5_background/layers/2_second_layer/2.png", 720, 80),
+    new BackgroundObject("assets/img/5_background/layers/1_first_layer/2.png", 720, 80),
+
+    new BackgroundObject("assets/img/5_background/layers/air.png", 720*2, 80),
+    new BackgroundObject("assets/img/5_background/layers/3_third_layer/1.png", 720*2, 80),
+    new BackgroundObject("assets/img/5_background/layers/2_second_layer/1.png", 720*2, 80),
+    new BackgroundObject("assets/img/5_background/layers/1_first_layer/1.png", 720*2, 80),
+
+    new BackgroundObject("assets/img/5_background/layers/air.png", 720*3, 80),
+    new BackgroundObject("assets/img/5_background/layers/3_third_layer/2.png", 720*3, 80),
+    new BackgroundObject("assets/img/5_background/layers/2_second_layer/2.png", 720*3, 80),
+    new BackgroundObject("assets/img/5_background/layers/1_first_layer/2.png", 720*3, 80),
+
+    new BackgroundObject("assets/img/5_background/layers/air.png", 720*4, 80),
+    new BackgroundObject("assets/img/5_background/layers/3_third_layer/1.png", 720*4, 80),
+    new BackgroundObject("assets/img/5_background/layers/2_second_layer/1.png", 720*4, 80),
+    new BackgroundObject("assets/img/5_background/layers/1_first_layer/1.png", 720*4, 80),
+
+    new BackgroundObject("assets/img/5_background/layers/air.png", 720*5, 80),
+    new BackgroundObject("assets/img/5_background/layers/3_third_layer/2.png", 720*5, 80),
+    new BackgroundObject("assets/img/5_background/layers/2_second_layer/2.png", 720*5, 80),
+    new BackgroundObject("assets/img/5_background/layers/1_first_layer/2.png", 720*5, 80),
+
+    new BackgroundObject("assets/img/5_background/layers/air.png", 720*6, 80),
+    new BackgroundObject("assets/img/5_background/layers/3_third_layer/1.png", 720*6, 80),
+    new BackgroundObject("assets/img/5_background/layers/2_second_layer/1.png", 720*6, 80),
+    new BackgroundObject("assets/img/5_background/layers/1_first_layer/1.png", 720*6, 80),
+
     ],
     [
         new Cloud()
