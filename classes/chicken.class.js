a9f08fd4195e3b7d1b6bbed677a9d83309fb2733
Tunc-Senel <senel.tunc@gmail.com>
2026-05-22 19:56:12 +0200

    feat: expand enemy setup and adjust spawn ranges

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -25,7 +25,7 @@ class Chicken extends MovableObject {
         super().loadImage("assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.DEAD_IMAGE);
-        this.x = 600 + (Math.random() * 500);
+        this.x = 800 + (Math.random() * 2500);
         this.speed = 0.15 + Math.random() * 0.25;
         this.animate();
     }
--- classes/small-chicken.class.js
+++ classes/small-chicken.class.js
@@ -25,7 +25,7 @@ class SmallChicken extends MovableObject {
         super().loadImage("assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
         this.loadImages(this.IMAGE_DEAD);
         this.loadImages(this.IMAGES_WALKING);
-        this.x = 200 + (Math.random() * 500);
+        this.x = 500 + (Math.random() * 1500);
         this.speed = 0.15 + Math.random() * 0.25;
         this.animate();
     }
